6cad95502e7d4a6c548b824cb3530a6ddb14a398
Robert345-th <zulur331@gmail.com>
2026-09-10 09:28:22 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@ const CACHE_NAME = 'aviator-odds-shell-v1';
 const SHELL_FILES = [
   '/',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
+  '/icon-192.png',
+  '/icon-512.png',
 ];
 
 self.addEventListener('install', (event) => {
